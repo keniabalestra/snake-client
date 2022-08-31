6059d7bab6346264da24baedc94afb76254a4f9a
Kenia Balestra <kenia.balestra@gmail.com>
2022-08-31 18:16:06 +0000

    play file updated with modules

---
+++ client.js
@@ -0,0 +1,33 @@
+const net = require("net");
+
+// establishes a connection with the game server
+const connect = function() {
+  const conn = net.createConnection({
+    host: "10.0.2.15",
+    port: 50541
+  });
+
+  // interpret incoming data as text
+  conn.setEncoding("utf8");
+  conn.on("connect", () => {
+    conn.write("Hello from Kenia!");
+  });
+
+  // code that does something when the connection is first established
+  conn.on("data", (info) => {
+    console.log("Data received!");
+    console.log(info);
+  });
+
+  //Standard input from Node.js
+  //When you type into terminal and hit enter.
+  process.stdin.on('data', (userMessage) => {
+    conn.write(userMessage);
+  });
+
+  return conn;
+};
+
+
+
+module.exports = {connect};
--- play.js
+++ play.js
@@ -1,33 +1,3 @@
-const net = require("net");
-
-// establishes a connection with the game server
-const connect = function() {
-  const conn = net.createConnection({
-    host: "10.0.2.15",
-    port: 50541
-  });
-  conn.on("data", (data) => {
-    console.log(data);
-  });
-  // conn.on("connect", () => {
-  //   conn.write("Hello from Kenia!");
-  // });
-
-  //Standard input from Node.js
-  //When you type into terminal and hit enter.
-  process.stdin.on('data', (userMessage) => {
-    conn.write(userMessage);
-  });
-
-  // interpret incoming data as text
-  conn.setEncoding("utf8");
-
-  return conn;
-};
-
-
-
-
-
+const { connect } = require("./client");
 console.log("Connecting ...");
 connect();
