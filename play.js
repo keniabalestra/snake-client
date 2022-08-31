c47171afb56515faea5565ba2d69ffb005141a33
Kenia Balestra <kenia.balestra@gmail.com>
2022-08-31 18:55:27 +0000

    play updated with handleUserInput

--- play.js
+++ play.js
@@ -1,3 +1,23 @@
 const { connect } = require("./client");
 console.log("Connecting ...");
 connect();
+// setup interface to handle user input from stdin
+
+const handleUserInput = function(key) {
+    // \u0003 maps to ctrl+c input
+    if (key === '\u0003') {
+      process.exit();
+    }
+};
+
+const setupInput = function() {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding("utf8");
+  stdin.resume();
+
+  stdin.on("data", handleUserInput);
+
+  return stdin;
+};
+setupInput();
